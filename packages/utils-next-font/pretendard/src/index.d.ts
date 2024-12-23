fe6d2e8974a42728dfeda6252e1ec395bca49dca
kang-heewon <heewon.dev@gmail.com>
2024-12-23 20:42:49 +0900

    feat: next 15 지원을 추가한다

--- packages/utils-next-font/pretendard/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import * as next_dist_compiled__next_font from 'next/dist/compiled/@next/font';
-
-declare const pretendardFont: next_dist_compiled__next_font.NextFont;
-
-export { pretendardFont };
--- packages/utils-next-font/pretendard/src/index.mjs
+++
@@ -1,5 +0,0 @@
-import r from 'next/font/local';
-
-const a=r({src:"../PretendardVariable.woff2",display:"swap",weight:"45 920"});
-
-export { a as pretendardFont };
