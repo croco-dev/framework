449961d3fbd3bf71256ca9f395fb11f7cd44748d
kang-heewon <heewon.dev@gmail.com>
2026-03-01 02:35:00 +0900

    refactor: share REST metadata keys across packages (#19 #20)

--- packages/protocols-rest/src/libs/constants.ts
+++ packages/protocols-rest/src/libs/constants.ts
@@ -1,11 +1,11 @@
-export const REST_CONTROLLER_KEY = Symbol('croco:rest:controller');
-export const REST_ROUTES_KEY = Symbol('croco:rest:routes');
-export const REST_PARAMS_KEY = Symbol('croco:rest:params');
-export const REST_GUARDS_KEY = Symbol('croco:rest:guards');
-export const REST_PIPES_KEY = Symbol('croco:rest:pipes');
-export const REST_INTERCEPTORS_KEY = Symbol('croco:rest:interceptors');
-export const REST_FILTERS_KEY = Symbol('croco:rest:filters');
-export const REST_ROLES_KEY = Symbol('croco:rest:roles');
+export const REST_CONTROLLER_KEY = Symbol.for('croco:rest:controller');
+export const REST_ROUTES_KEY = Symbol.for('croco:rest:routes');
+export const REST_PARAMS_KEY = Symbol.for('croco:rest:params');
+export const REST_GUARDS_KEY = Symbol.for('croco:rest:guards');
+export const REST_PIPES_KEY = Symbol.for('croco:rest:pipes');
+export const REST_INTERCEPTORS_KEY = Symbol.for('croco:rest:interceptors');
+export const REST_FILTERS_KEY = Symbol.for('croco:rest:filters');
+export const REST_ROLES_KEY = Symbol.for('croco:rest:roles');
 
 export enum HttpMethod {
   GET = 'GET',
