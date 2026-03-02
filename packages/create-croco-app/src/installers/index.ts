a15337bd94dde69a3b2701cd43f102b5329bd94d
kang-heewon <heewon.dev@gmail.com>
2026-03-02 12:48:05 +0900

    feat(create-croco-app): add web-trpc addon template and installer

--- packages/create-croco-app/src/installers/index.ts
+++ packages/create-croco-app/src/installers/index.ts
@@ -5,3 +5,4 @@ export { installSharedUi } from './shared-ui.js';
 export { installTrpcNextjs } from './trpc-nextjs.js';
 export { installTrpcStandalone } from './trpc-standalone.js';
 export { installWebGraphql } from './web-graphql.js';
+export { installWebTrpc } from './web-trpc.js';
