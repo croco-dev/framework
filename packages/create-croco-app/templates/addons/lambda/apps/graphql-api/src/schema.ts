9ec9fb4cf60e2c60989a13f15ab3500308539e4d
kang-heewon <heewon.dev@gmail.com>
2026-03-07 13:40:02 +0900

    fix: add telemetry wiring to lambda graphql template (#233)

---
+++ packages/create-croco-app/templates/addons/lambda/apps/graphql-api/src/schema.ts
@@ -0,0 +1,17 @@
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+
+export async function createSchema(): Promise<GraphQLSchema> {
+  const query = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      health: {
+        type: GraphQLString,
+        resolve: () => 'ok',
+      },
+    },
+  });
+
+  return new GraphQLSchema({
+    query,
+  });
+}
