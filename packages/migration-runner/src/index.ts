ab713e09e4a1219c65ecae4c345a1d30e5d6ddbf
kang-heewon <heewon.dev@gmail.com>
2026-05-06 12:28:15 +0900

    fix(migration-runner): export database client types

--- packages/migration-runner/src/index.ts
+++ packages/migration-runner/src/index.ts
@@ -1,7 +1,7 @@
+export type { DatabaseClient } from './libs/db-types';
 export { MigrationRunner } from './libs/MigrationRunner';
 export { MigrationScanner } from './libs/MigrationScanner';
 export { MigrationStore } from './libs/MigrationStore';
-export type { DatabaseClient } from './libs/db-types';
 export type {
   MigrationDirection,
   MigrationFile,
